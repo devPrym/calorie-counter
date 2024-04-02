9f00a8ce121a8457ce7e498f1e89b745867f640b
devPrym <devprym@gmail.com>
2024-04-02 18:31:23 -0500

    added clean and invalid input functions / added the entry function

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
+
+function cleanInputString(str) {
+    const regex = /[+-\s]/g;
+    return str.replace(regex, '');
+}
+  
+function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+}
+
+function addEntry() {
+    const targetId = '#' + entryDropdown.value;
+    const targetInputContainer = document.querySelector(targetId + ' .input-container');
+}
